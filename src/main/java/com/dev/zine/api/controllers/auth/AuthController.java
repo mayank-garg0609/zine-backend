3e2cc779c9e51cb6e6a937d6ff5f5a7294561959
RUPESH <89046490+rupesh031@users.noreply.github.com>
2023-12-22 19:55:23 +0530

    login and register

--- src/main/java/com/dev/zine/api/controllers/auth/AuthController.java
+++ src/main/java/com/dev/zine/api/controllers/auth/AuthController.java
@@ -3,22 +3,27 @@ package com.dev.zine.api.controllers.auth;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dev.zine.api.controllers.model.RegistrationBody;
+import com.dev.zine.api.model.LoginBody;
+import com.dev.zine.api.model.LoginResponse;
+import com.dev.zine.api.model.RegistrationBody;
 import com.dev.zine.exceptions.UserAlreadyExistsException;
-import com.dev.zine.service.userService;
+import com.dev.zine.model.User;
+import com.dev.zine.service.UserService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private userService userService;
+    private UserService userService;
 
-    public AuthController(userService userService) {
+    public AuthController(UserService userService) {
         this.userService = userService;
     }
 
@@ -37,6 +42,23 @@ public class AuthController {
         } catch (UserAlreadyExistsException ex) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginBody loginBody) {
+        String jwt = userService.loginUser(loginBody);
+        if (jwt == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            LoginResponse response = new LoginResponse();
+            response.setJwt(jwt);
+            return ResponseEntity.ok(response);
+        }
+    }
+
+    @GetMapping("/me")
+    public User getLoggedInUserProfile(@AuthenticationPrincipal User user) {
+        return user;
     }
 
 }
