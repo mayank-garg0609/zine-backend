6e5925e819b8382be5ce28db217ae6163b52d8f2
herschelleT <herschellethomas10@gmail.com>
2024-07-26 18:54:02 +0530

    fixed cors issue

--- src/main/java/com/dev/zine/api/security/WebSocketsConfig.java
+++ src/main/java/com/dev/zine/api/security/WebSocketsConfig.java
@@ -3,23 +3,24 @@ package com.dev.zine.api.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@EnableScheduling
 public class WebSocketsConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/room/");
         config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/room");
+        config.setUserDestinationPrefix("/user");
     }
 }
