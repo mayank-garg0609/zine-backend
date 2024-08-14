289b87819d6820030de73e1ee52b691314df1aac
herschelleT <herschellethomas10@gmail.com>
2024-08-14 23:41:46 +0530

    duplicate mentor bug fix

--- src/main/java/com/dev/zine/dao/TaskMentorDAO.java
+++ src/main/java/com/dev/zine/dao/TaskMentorDAO.java
@@ -4,8 +4,11 @@ import org.springframework.data.repository.ListCrudRepository;
 
 import com.dev.zine.model.Task;
 import com.dev.zine.model.TaskMentor;
+import com.dev.zine.model.User;
+
 import java.util.List;
 
 public interface TaskMentorDAO extends ListCrudRepository<TaskMentor, Long>{
     List<TaskMentor> findByTaskId(Task taskId);
+    boolean existsByMentorAndTaskId(User user, Task taskId);
 }
