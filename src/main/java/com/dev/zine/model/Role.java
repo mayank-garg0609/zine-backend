677c13cd0cab0982978aa2f6c0412c357d772582
herschelleT <herschellethomas10@gmail.com>
2024-08-17 19:18:45 +0530

    roles cascade

--- src/main/java/com/dev/zine/model/Role.java
+++ src/main/java/com/dev/zine/model/Role.java
@@ -1,10 +1,16 @@
 package com.dev.zine.model;
 
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +27,8 @@ public class Role {
 
     @Column(name = "permission")
     private String permission;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserToRole> userToRoles = new ArrayList<>();
 }
