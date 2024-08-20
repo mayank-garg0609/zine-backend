81a7e1be9ba5a7d5137690723979930b3bbda2c8
Herschelle Thomas <herschellethomas10@gmail.com>
2024-08-20 17:06:57 +0530

    blog update & delete

--- src/main/java/com/dev/zine/service/BlogService.java
+++ src/main/java/com/dev/zine/service/BlogService.java
@@ -4,24 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.cloud.Timestamp;
+
+import java.lang.reflect.InvocationTargetException;
 import java.time.Instant;
 import java.util.List;
 
-import com.dev.zine.dao.BlogComponentDAO;
 import com.dev.zine.dao.BlogDAO;
-import com.dev.zine.model.BlogComponent;
 import com.dev.zine.model.Blog;
-
+import com.dev.zine.utils.NullAwareBeanUtilsBean;
 import com.dev.zine.api.model.blogs.BlogBody;
-import com.dev.zine.api.model.blogs.BlogComponentBody;
 import com.dev.zine.exceptions.BlogNotFound;
 
 @Service
 public class BlogService {
     @Autowired
     private BlogDAO blogDAO;
-    @Autowired
-    private BlogComponentDAO blogComponentDAO;
 
     public Blog createBlog(BlogBody blogBody) throws BlogNotFound{
         Blog newBlog = new Blog();
@@ -29,10 +26,11 @@ public class BlogService {
         if(blogBody.getParentBlog() != null){
             parentBlog = blogDAO.findById(blogBody.getParentBlog()).orElseThrow(() -> new BlogNotFound(blogBody.getParentBlog()));
         }
-        newBlog.setBlogName(blogBody.getTitle());
-        newBlog.setBlogDescription(blogBody.getDescription());
+        newBlog.setBlogName(blogBody.getBlogName());
+        newBlog.setBlogDescription(blogBody.getBlogDescription());
         newBlog.setCreatedAt(Timestamp.ofTimeSecondsAndNanos(Instant.now().getEpochSecond(), Instant.now().getNano()));
-        newBlog.setDpURL(blogBody.getDp());
+        newBlog.setContent(blogBody.getContent());
+        newBlog.setDpURL(blogBody.getDpURL());
         newBlog.setParentBlog(parentBlog);
         blogDAO.save(newBlog);
         return newBlog;
@@ -46,26 +44,26 @@ public class BlogService {
             return blogDAO.findByParentBlog(parentBlog);
         }
     }
-    // public void createBlogComponent(BlogComponentBody blogComponentBody) throws Exception{
-    //     BlogComponent newComponent = new BlogComponent();
-    //     Blog parentBlog = blogDAO.findById(blogComponentBody.getBlog_id()).orElseThrow(() -> new BlogNotFound(blogComponentBody.getBlog_id()));
-    //     newComponent.setComponentType(blogComponentBody.getComponent_type());
-    //     newComponent.setContent(blogComponentBody.getContent());
-    //     newComponent.setRelativeOrder(blogComponentBody.getOrder());
-    //     newComponent.setBlog(parentBlog);
-    //     blogComponentDAO.save(newComponent);
-    //     // try {
-    //     // } catch(Exception e){
-    //     //     throw new Exception("Could not create component");
-    //     // }
-    // }
-    // public List<BlogComponent> getBlogComponents(Long BlogID) throws Exception{
-    //     Blog parentBlog = blogDAO.findByBlogID(BlogID);
-    //     return blogComponentDAO.findByBlog(parentBlog);
-    //     // try {
-    //     // } catch(Exception e){
-    //     //     throw new Exception("Could not create component");
-    //     // }
-    // }
-    
+
+    public Blog updateBlog(Long blogId, BlogBody update) throws BlogNotFound{
+        Blog existingBlog = blogDAO.findById(blogId).orElseThrow(() -> new BlogNotFound(blogId));
+        try{
+            NullAwareBeanUtilsBean beanUtilsBean = new NullAwareBeanUtilsBean();
+            beanUtilsBean.copyProperties(existingBlog, update);
+            blogDAO.save(existingBlog);
+            return existingBlog;
+        } catch(IllegalAccessException | InvocationTargetException e){
+            return existingBlog;
+        }
+    }
+
+    public boolean deleteBlog(List<Long> blogIds){
+        try{
+            blogDAO.deleteAllById(blogIds);
+            return true;
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
