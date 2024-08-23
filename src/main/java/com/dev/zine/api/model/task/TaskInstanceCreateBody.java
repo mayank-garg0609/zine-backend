d2edacf1ef6ddf45bd09a718dc23abf4d7231e16
Herschelle Thomas <herschellethomas10@gmail.com>
2024-08-23 21:37:45 +0530

    added instance name

--- src/main/java/com/dev/zine/api/model/task/TaskInstanceCreateBody.java
+++ src/main/java/com/dev/zine/api/model/task/TaskInstanceCreateBody.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TaskInstanceCreateBody {
     private String type;
-    private String roomName;
+    private String roomInstanceName;
     private String dpUrl;
     private String description;
 }
