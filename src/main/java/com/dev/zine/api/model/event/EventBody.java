567fb1c75f56595618d87df13791efa6ba03fb33
Herschelle Thomas <herschellethomas10@gmail.com>
2024-08-28 23:31:21 +0530

    event create validation

--- src/main/java/com/dev/zine/api/model/event/EventBody.java
+++ src/main/java/com/dev/zine/api/model/event/EventBody.java
@@ -6,15 +6,29 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.NotNull;
+
 @Data
 public class EventBody {
+    @NotNull
     private String description;
+
+    @NotNull
     private String type;
+
+    @NotNull
     private String name;
+    
+    @NotNull
     private String venue;
+
+    @NotNull
     private Long recruitment;
+
+    @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startDateTime;
+    
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endDateTime;
 }
--- src/main/java/com/dev/zine/model/Event.java
+++ src/main/java/com/dev/zine/model/Event.java
@@ -1,40 +1,50 @@
 package com.dev.zine.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 @Table(name = "Event")
 @Data
 @NoArgsConstructor
-
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name ="id" , nullable = false)
     private long id;
 
+    @NotNull
     @Column(name = "description")
     private String description;
 
+    @NotNull
     @Column(name = "type")
     private String type;
 
+    @NotNull
     @Column(name = "name")
     private String name;
 
+    @NotNull
     @Column(name = "venue")
     private String venue;
 
+    @NotNull
     @Column(name ="startDate")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date startDateTime;
 
     @Column(name = "endDate")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date endDateTime;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name="recruitment")
     private Recruitment recruitment;
