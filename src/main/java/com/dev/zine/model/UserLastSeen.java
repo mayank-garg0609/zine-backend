39702a8611ef9d7494a7852bb0c62145a7339dd4
Herschelle Thomas <herschellethomas10@gmail.com>
2024-09-08 02:07:49 +0530

    last-seen

---
+++ src/main/java/com/dev/zine/model/UserLastSeen.java
@@ -0,0 +1,41 @@
+package com.dev.zine.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_last_seen")
+@NoArgsConstructor
+@Data
+public class UserLastSeen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Rooms room;
+
+    private Timestamp lastSeen;
+
+    public UserLastSeen(User user, Rooms room) {
+        this.user = user;
+        this.room = room;
+    }
+}
+
