22b1d504895fb5e96096c769ccfab9a21b3ac9ef
Herschelle Thomas <herschellethomas10@gmail.com>
2024-09-08 17:45:18 +0530

    room last seen info+

--- src/main/java/com/dev/zine/service/UserLastSeenService.java
+++ src/main/java/com/dev/zine/service/UserLastSeenService.java
@@ -6,11 +6,14 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dev.zine.api.model.user.RoomLastSeenInfo;
+import com.dev.zine.dao.MessagesDAO;
 import com.dev.zine.dao.RoomsDAO;
 import com.dev.zine.dao.UserDAO;
 import com.dev.zine.dao.UserLastSeenDAO;
 import com.dev.zine.exceptions.RoomDoesNotExist;
 import com.dev.zine.exceptions.UserNotFound;
+import com.dev.zine.model.Message;
 import com.dev.zine.model.Rooms;
 import com.dev.zine.model.User;
 import com.dev.zine.model.UserLastSeen;
@@ -24,6 +27,8 @@ public class UserLastSeenService {
     private UserDAO userDAO;
     @Autowired
     private RoomsDAO roomsDAO;
+    @Autowired
+    private MessagesDAO messagesDAO;
 
     public void updateLastSeen(String userEmail, Long roomId) throws UserNotFound, RoomDoesNotExist{
         User user = userDAO.findByEmailIgnoreCase(userEmail).orElseThrow(() -> new UserNotFound());
@@ -40,4 +45,25 @@ public class UserLastSeenService {
                          .map(UserLastSeen::getLastSeen)
                          .orElse(null);
     }
+
+    public Timestamp getLastSeen(User user, Rooms room) throws UserNotFound, RoomDoesNotExist{
+        return userLastSeenDAO.findByUserAndRoom(user, room)
+                         .map(UserLastSeen::getLastSeen)
+                         .orElse(null);
+    }
+
+    public RoomLastSeenInfo getRoomLastSeenInfo(String userEmail, Long roomId) throws UserNotFound, RoomDoesNotExist{
+        User user = userDAO.findByEmailIgnoreCase(userEmail).orElseThrow(() -> new UserNotFound());
+        Rooms room = roomsDAO.findById(roomId).orElseThrow(() -> new RoomDoesNotExist());
+        RoomLastSeenInfo body = new RoomLastSeenInfo();
+        Timestamp lastSeen = getLastSeen(user, room);
+        Timestamp lastMessageTimestamp = messagesDAO.findFirstByRoomIdOrderByTimestampDesc(room).map(Message::getTimestamp).orElse(null);
+        Long unreadMessages = messagesDAO.countUnreadMessages(room, lastSeen);
+        body.setLastMessageTimestamp(lastMessageTimestamp);
+        body.setUnreadMessages(unreadMessages);
+        body.setUserLastSeen(lastSeen);
+        return body;
+    }
+
+
 }
