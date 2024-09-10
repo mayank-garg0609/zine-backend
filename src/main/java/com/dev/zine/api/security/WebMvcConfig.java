a3b205b895d6cf289e809858670a87e610c62f4d
Herschelle Thomas <herschellethomas10@gmail.com>
2024-09-11 01:41:19 +0530

    webmvc favicon config

--- src/main/java/com/dev/zine/api/security/WebMvcConfig.java
+++ src/main/java/com/dev/zine/api/security/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.dev.zine.api.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOriginPatterns("*")
                     .allowedMethods("POST", "DELETE", "PUT", "GET", "PATCH");
+    } 
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600)
+                .resourceChain(true);
     }
 }
