4ba4cd021eaf43c9c994e529ca669c4e5526eb50
Herschelle Thomas <herschellethomas10@gmail.com>
2024-12-24 12:15:34 +0530

    media dao models

---
+++ src/main/java/com/dev/zine/api/model/images/ImagesUploadRes.java
@@ -0,0 +1,10 @@
+package com.dev.zine.api.model.images;
+
+import lombok.Data;
+
+@Data
+public class ImagesUploadRes {
+    private String url;
+    private String publicId;
+    private String message;
+}
