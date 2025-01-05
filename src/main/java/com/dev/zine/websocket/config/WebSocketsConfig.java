a5c4cf9ecca96685a41ef0b79b506b4f5d0a1670
Herschelle Thomas <herschellethomas10@gmail.com>
2025-01-05 11:19:03 +0530

    added active users functionality to websocket

--- src/main/java/com/dev/zine/websocket/config/WebSocketsConfig.java
+++ src/main/java/com/dev/zine/websocket/config/WebSocketsConfig.java
@@ -1,4 +1,4 @@
-package com.dev.zine.api.security;
+package com.dev.zine.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
