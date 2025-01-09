82839efd6b4d68f78218a14e1a7bbc463b81708b
Herschelle Thomas <herschellethomas10@gmail.com>
2025-01-09 16:25:49 +0530

    get unfilled form ids

--- src/main/java/com/dev/zine/dao/form/FormDAO.java
+++ src/main/java/com/dev/zine/dao/form/FormDAO.java
@@ -9,8 +9,8 @@ import org.springframework.data.repository.query.Param;
 import com.dev.zine.model.form.Form;
 
 public interface FormDAO extends ListCrudRepository<Form, Long> {
-    @Query("SELECT f FROM Form f WHERE NOT EXISTS (" +
+    @Query("SELECT f.id FROM Form f WHERE NOT EXISTS (" +
        "SELECT r FROM Response r JOIN r.question q WHERE q.form = f AND r.user.id = :userId)")
-    List<Form> findFormsUserHasNotFilled(@Param("userId") Long userId);
+    List<Long> findFormsUserHasNotFilled(@Param("userId") Long userId);
 
 }
