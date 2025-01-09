384d502377855bd4da22a3a2c534f53547f7f6fa
Herschelle Thomas <herschellethomas10@gmail.com>
2025-01-09 21:56:23 +0530

    add 2024 to workshop rooms on registration

--- src/main/java/com/dev/zine/api/model/roomMembers/Members.java
+++ src/main/java/com/dev/zine/api/model/roomMembers/Members.java
@@ -1,9 +1,11 @@
 package com.dev.zine.api.model.roomMembers;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Members {
     @NotNull
     private String userEmail;
