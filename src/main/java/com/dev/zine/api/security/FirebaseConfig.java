5e52a87634d2ceca7d589559f87cd02213c1fd59
Mayank Garg <157978799+mayank-garg0609@users.noreply.github.com>
2025-10-19 23:46:57 +0530

    Update FirebaseConfig to use absolute path for all envs

--- src/main/java/com/dev/zine/api/security/FirebaseConfig.java
+++ src/main/java/com/dev/zine/api/security/FirebaseConfig.java
@@ -30,7 +30,7 @@ public class FirebaseConfig {
         if(env.equals("production")) {
             absolutePath = "/etc/secret/zine-firebase-admin.json";
         } else{
-            String relativePath = "src/main/resources/zine-firebase-admin.json";
+            String relativePath = "/etc/secret/zine-firebase-admin.json";
             absolutePath = new File(relativePath).getAbsolutePath();
         }
       
